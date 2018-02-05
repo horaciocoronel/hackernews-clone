021f97d8425bf21894ab56598b331c1269505130
horaciocoronel <arielc@gmail.com>
2018-02-05 14:11:30 -0500

    Create function to iterate through article ids and append them to the main tag, parse url to display hostname

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ function hackerNewsTopStories() {
            if (xmlhttp.status == 200) {
 							var topStoriesArr = JSON.parse(xmlhttp.responseText);
 							console.log(topStoriesArr);
-							 return topStoriesArr;
+							topStoriesArr.forEach(function(element) {
+								// Executes the getArticleInfo function passing the article ID
+								getArticleInfo(element);
+							});
+							 // return xmlhttp.responseText;
            }
            else if (xmlhttp.status == 400) {
               alert('There was an error 400');
@@ -20,5 +24,52 @@ function hackerNewsTopStories() {
     xmlhttp.open("GET", "https://hacker-news.firebaseio.com/v0/topstories.json", true);
     xmlhttp.send();
 }
+
+var position = 0
+function getArticleInfo(articleId) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+				 if (xmlhttp.status == 200) {
+					 var response = JSON.parse(xmlhttp.responseText);
+					 var article = document.createElement("article");
+           article.innerHTML = `
+						 <div class="article-position">
+		 					<span class="rank">${position + 1}.</span>
+		 					<span class="arrow"></span>
+		 				</div>
+		 				<div class="article-title">
+		 					<span class="title"><a href="${response.url}" target="_blank">${response.title}</a> </span>
+		 					<span class="source">(${parseLocation(`${response.url}`)})</span>
+		 					<div class="article-subtitle">
+		 						<span class="score">${response.score}</span> points by <span class="author">${response.by}</span> <span class="posted">2 hours ago</span> | hide | <span class="comments">${response.descendants}</span> comments
+		 					</div>
+		 				</div>
+					 `
+
+					 var main = document.querySelector("main");
+					 main.appendChild(article, main);
+
+				 }
+				 position += 1;
+			 }
+
+		 };
+		 xmlhttp.open("GET", "https://hacker-news.firebaseio.com/v0/item/"+articleId+".json?", true);
+		 xmlhttp.send();
+}
+
+var parseLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+		var parsedUrl = l.hostname;
+		parsedUrl.replace('www.','')
+		console.log(parsedUrl);
+    return parsedUrl;
+};
+
+
+
+
 
 hackerNewsTopStories();
